41eb7656a69dd56af80cabfb93281f6ae6f20622
ankita222004 <sahuankita3285@gmail.com>
2023-03-30 12:13:46 +0530

    merge other

---
+++ C (fundamental programming)/Addition.c
@@ -0,0 +1,14 @@
+//WAP to print addition of two numbers.
+
+#include<stdio.h>
+
+int main()
+{
+	int a,b;
+	printf("Enter two numbers:");
+	scanf("%d %d",&a,&b);
+	
+	printf("The addition of two numbers=%d",a+b);
+	
+	return 0;
+}
---
+++ C (fundamental programming)/Task_1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("---------------------MENU---------------------");
+	printf("\n Product \t");
+	printf("Price");
+	printf("\n 1.Grapes\t90/-");
+	printf("\n 2.Mango\t120/-");
+	printf("\n 3.Banana\t40/-");
+	printf("\n 4.Kiwi\t\t60/-");
+	printf("\n===============================================");
+	
+	printf("\n--------------------YOUR DETAILS--------------------");
+	printf("\nEnter name :\tAnkita Sahu");
+	printf("\nEnter DOB :\t2004/04/22");
+	printf("\nEnter E-mail :\tsahuankita3285@gmail.com");
+	printf("\nEnter Age :\t18");
+	printf("\nEnter Gender :\tFemale");
+	printf("\nEnter Hobbies :\treading comics");
+	printf("\nAddress :\tC-26 Tirthbhumi tenament-II Behind mony hotel,Isanpur naroda highway,Ahemdabad");
+	printf("\nMobile No- :\t7405114401");
+}
