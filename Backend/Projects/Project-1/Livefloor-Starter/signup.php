86addb959acae47fec57a6e068d10fe51f9547b8
ankita222004 <sahuankita3285@gmail.com>
2023-06-26 20:41:51 +0530

    merge other

---
+++ Backend/Projects/Project-1/Livefloor-Starter/signup.php
@@ -0,0 +1,128 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+-->
+<!doctype html>
+<html lang="zxx">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Livefloor an Interior & Furniture Category Bootstrap Responsive Template | Contact :: W3layouts</title>
+  <!-- google fonts -->
+  <link href="//fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+  <!-- google fonts -->
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="assets/css/style-starter.css">
+  <!-- Template CSS -->
+</head>
+
+<body>
+  <!--header-->
+  <?php
+  include_once('header.php');
+  ?>
+  <!-- //header -->
+  <!-- about breadcrumb -->
+  <section class="w3l-about-breadcrumb text-left">
+    <div class="breadcrumb-bg breadcrumb-bg-about py-sm-5 py-4">
+      <div class="container">
+        <h2 class="title">Sign Up</h2>
+        <ul class="breadcrumbs-custom-path mt-2">
+          <li><a href="#url">Home</a></li>
+          <li class="active"><span class="fa fa-arrow-right mx-2" aria-hidden="true"></span> Sign Up </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  <!-- //about breadcrumb -->
+   <!-- /contact-->
+  <div class="contact-form py-5" id="contact">
+    <div class="container py-lg-5 py-md-4">
+      
+      <h3 class="hny-title mb-lg-5 mb-4">Sign Up</h3>
+        <div class="contacts12-main mb-5">
+            <form action="/action_page.php">
+				<div class="mb-3 mt-3">
+					<label for="email" class="form-label">First Name:</label>
+					<input type="email" class="form-control" id="email" placeholder="Enter Firstname" name="email">
+				</div>
+				<div class="mb-3">
+					<label for="pwd" class="form-label">Last Name:</label>
+					<input type="password" class="form-control" id="pwd" placeholder="Enter Lastname" name="pswd">
+				</div>
+				<div class="mb-3 mt-3">
+					<label for="email" class="form-label">Email:</label>
+					<input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+				</div>
+				<div class="mb-3">
+					<label for="pwd" class="form-label">Password:</label>
+					<input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+				</div>
+				<div class="mb-3">
+					<label for="pwd" class="form-label">Confirm Password:</label>
+					<input type="password" class="form-control" id="pwd" placeholder="Confirm password" name="pswd">
+				</div>
+				<div class="form-check mb-3">
+					<label class="form-check-label"></label>
+				</div>
+				<button type="submit" class="btn btn-primary">Sign Up</button>
+			</form>
+        </div>
+    </div>
+  </div>
+
+<!--//contact-->
+  <!--/w3l-footer-29-main-->
+  <?php
+  include_once('footer.php');
+  ?>
+  <!-- //footer-29 block -->
+  <!-- disable body scroll which navbar is in active -->
+  <script>
+    $(function () {
+      $('.navbar-toggler').click(function () {
+        $('body').toggleClass('noscroll');
+      })
+    });
+  </script>
+  <!-- disable body scroll which navbar is in active -->
+
+  <!-- Template JavaScript -->
+  <script src="assets/js/jquery-3.3.1.min.js"></script>
+  <script src="assets/js/theme-change.js"></script>
+  <!--/MENU-JS-->
+  <script>
+    $(window).on("scroll", function () {
+      var scroll = $(window).scrollTop();
+
+      if (scroll >= 80) {
+        $("#site-header").addClass("nav-fixed");
+      } else {
+        $("#site-header").removeClass("nav-fixed");
+      }
+    });
+
+    //Main navigation Active Class Add Remove
+    $(".navbar-toggler").on("click", function () {
+      $("header").toggleClass("active");
+    });
+    $(document).on("ready", function () {
+      if ($(window).width() > 991) {
+        $("header").removeClass("active");
+      }
+      $(window).on("resize", function () {
+        if ($(window).width() > 991) {
+          $("header").removeClass("active");
+        }
+      });
+    });
+  </script>
+  <!--//MENU-JS-->
+
+  <script src="assets/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
