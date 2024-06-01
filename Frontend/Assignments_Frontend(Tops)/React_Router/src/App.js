64eb04ba0f3b29087c051b8f57d1803575b0df2c
ankita222004 <sahuankita3285@gmail.com>
2024-06-01 09:42:36 +0530

    merge other

--- Frontend/Assignments_Frontend(Tops)/React_Router/src/App.js
+++ Frontend/Assignments_Frontend(Tops)/React_Router/src/App.js
@@ -1,17 +1,31 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Dashboard from "./Admin/Pages/Dashboard";
-import Add_Mng_user from "./Admin/Pages/Add_Mng_user";
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as  Routes, Route } from 'react-router-dom';
 
+const Admin = lazy(() => import('./pages/Admin/Admin'));
+const User = lazy(() => import('./pages/Admin/User'));
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<><Dashboard/></>}></Route>
-          <Route path="/add_mng_user" element={<><Add_Mng_user/></>}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <div>
+          <nav>
+            <ul>
+              <li><link to="/">Home</link></li>
+              <li><link to="/admin">Admin</link></li>
+            </ul>
+          </nav>
+
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<div>Home Page</div>} />
+              <Route path="/admin" element={<Admin/>}>
+                  <Route path="user" element={<User />} />
+              </Route>
+            </Routes>
+          </Suspense>
+        </div>
+      </Routes>
     </div>
   );
 }
